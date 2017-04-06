22461654ecc2e4e2507dfc54d21d482d9be2b9e8
349032805@qq.com <349032805@qq.com>
2017-04-06 12:11:06 +0800

    git commit

--- app/scripts/directives/header.js
+++ app/scripts/directives/header.js
@@ -1,15 +1,13 @@
   'use strict';
   
-function postLink(scope, element, attrs) {
-  scope.title = attrs.title;
-}
-
 angular.module('eparty')
   .directive('weuiheader', function () {
   return {
     restrict: 'E',
   	templateUrl: '/views/header.html',
-  	link:postLink
+  	link:function postLink(scope, element, attrs){
+  		scope.title = attrs.title;
+  	}
   };
 
 });
