40751784a3acaea71b2faecace5c0c3b8ca5e759
349032805@qq.com <349032805@qq.com>
2017-04-12 18:42:52 +0800

    git commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,16 +2,21 @@
 
 angular.module('eparty')
   .controller('MainCtrl', function ($scope,$timeout,$rootScope) {
+  	$scope.showBig = false;
 
-  	 if(!$rootScope.hasShowed){
+  	if(!$rootScope.hasShowed){
   	 	$timeout(function() {
          $rootScope.hasShowed = true;
         }, 2000);
   	}
 
   	//显示大图
-	$scope.showBigImg = function () {
-     alert(111);
-   }
+  	$scope.showBigImg = function () {
+       $scope.showBig = true;
+    }
+
+    
+
+
     
   });
